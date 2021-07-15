a5515ab1591efcf2e462da0734bc9dcbc3006197
Kat <78382977+katrolbi@users.noreply.github.com>
2021-07-15 14:37:07 +0200

    Delete homework directory

--- homework/homework25.py
+++
@@ -1,9 +0,0 @@
-'''
-Użyj biblioteki psutil(https://pypi.org/project/psutil/) i napisz funkcję, która sprawdzi, ile procesorów
-ma komputer, na którym właśnie uruchamiany jest ten program. Dokumentacja psutil dostępna pod
-https://psutil.readthedocs.io/en/latest/
-'''
-
-import psutil
-
-print(psutil.cpu_count())
--- homework/homework28.py
+++
@@ -1,4 +0,0 @@
-'''Napisz program, który wyświetli tylko foldery, które znajdują się w bieżącym katalogu'''
-
-import os
-print(os.listdir('.'))
